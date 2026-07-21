c9c0eea6f818439ce4881a4f8d2e32190d613a6f
PRATHAMESH PIMPALE <prathameshpimpale07@gmail.com>
2026-07-21 15:44:56 +0000

    feat(backend): create Favorite model schema

---
+++ backend/models/Favorite.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const favoriteSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true, unique: true },
+  exerciseIds: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Exercise' }]
+}, { timestamps: true });
+
+module.exports = mongoose.model('Favorite', favoriteSchema);
