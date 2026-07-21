40d64749295926b51b7848cf5801f88dcd98cd97
PRATHAMESH PIMPALE <prathameshpimpale07@gmail.com>
2026-07-22 02:16:52 +0530

    update Google OAuth Client ID in frontend and backend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 import './index.css';
 import App from './App.jsx';
 
-const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || '65460293350-m76sjjtl647l4sk4vm3dq1ibuu4tj6m3.apps.googleusercontent.com';
+const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || '65460293350-81bn2cd3kjde7dnqsrtcqpfhcd2k67gk.apps.googleusercontent.com';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
